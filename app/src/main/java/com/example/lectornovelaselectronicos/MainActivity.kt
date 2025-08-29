e3446a7f2b6728612551a1608a8bb233452f4aa7
Ismael de la Rosa del Castillo <ismaeldlr10@gmail.com>
2025-08-28 18:11:45 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lectornovelaselectronicos/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.lectornovelaselectronicos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lectornovelaselectronicos.Fragmentos.Biblioteca
+import com.example.lectornovelaselectronicos.Fragmentos.Explorar
+import com.example.lectornovelaselectronicos.Fragmentos.Historial
+import com.example.lectornovelaselectronicos.Fragmentos.Cuenta
+import com.example.lectornovelaselectronicos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //VerFragmentBiblioteca()
+
+        binding.BottomNV.setOnItemSelectedListener { Item ->
+            when (Item.itemId) {
+                R.id.Item_Biblioteca->{
+                    //VerFragmentBiblioteca()
+                    true
+                }
+                R.id.Item_Explorar->{
+                    //VerFragmentExplorar()
+                    true
+                }
+                R.id.Item_Historial->{
+                    //VerFragmentHistorial()
+                    true
+                }
+                R.id.Item_Cuenta->{
+                    //VerFragmentCuenta()
+                    true
+                }
+                else->{
+                    false
+                }
+            }
+        }
+    }
+    private fun VerFragmentBiblioteca(){
+        binding.TituloRL.setText("Biblioteca")
+        val fragment = Biblioteca()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "Biblioteca").commit()
+        fragmentTransition.commit()
+    }
+
+    private fun VerFragmentExplorar(){
+        binding.TituloRL.setText("Explorar")
+        val fragment = Explorar()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "Explorar").commit()
+        fragmentTransition.commit()
+    }
+
+    private fun VerFragmentHistorial(){
+        binding.TituloRL.setText("Historial")
+        val fragment = Historial()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "Historial").commit()
+        fragmentTransition.commit()
+    }
+
+    private fun VerFragmentCuenta() {
+        binding.TituloRL.setText("Cuenta")
+        val fragment = Cuenta()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "Cuenta").commit()
+        fragmentTransition.commit()
+    }
+}
