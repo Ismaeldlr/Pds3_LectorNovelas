6d14537c39150ff0f0f986ce0530e31b34c19993
Ismael de la Rosa del Castillo <ismaeldlr10@gmail.com>
2025-09-01 17:13:52 -0700

    Added google button on login

--- app/src/main/java/com/example/lectornovelaselectronicos/Login_email.kt
+++ app/src/main/java/com/example/lectornovelaselectronicos/Login_email.kt
@@ -9,12 +9,18 @@ import androidx.core.view.WindowInsetsCompat
 class Login_email : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_login_email)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        findViewById<com.google.android.gms.common.SignInButton>(R.id.btnGoogle)
+            .setSize(com.google.android.gms.common.SignInButton.SIZE_ICON_ONLY)
     }
 }
